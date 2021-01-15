2ae36dc09f96e96356661c4e748dd90d8a715308
Daniel5571 <dvernola@calpoly.edu>
2021-01-15 12:41:02 -0800

    still got last function to do

--- base_convert.py
+++ base_convert.py
@@ -1,3 +1,24 @@
-
+#input is number and base, output is string consisting of numbers
+#takes a number and does a base conversion
 def convert(num, b):
-    """Recursive function that returns a string representing num in the base b"""
+    newstring = ''
+    if num // b == 0:                 #base case
+        return newstring + str(num % b)                           #add remainder to string
+    quotient = num // b
+    remainder = num % b
+    if remainder == 10:
+        remainder = 'A'
+    elif remainder == 11:
+        remainder = 'B'
+    elif remainder == 12:
+        remainder = 'C'
+    elif remainder == 13:
+        remainder = 'D'
+    elif remainder == 14:
+        remainder = 'E'
+    elif remainder == 15:
+        remainder = 'F'
+    newstring = newstring + str(remainder)
+    return convert(quotient, b) + newstring
+
+print(convert(45, 2))
--- perm_lex.py
+++ perm_lex.py
@@ -1,3 +1,17 @@
-def perm_gen_lex(a): 
+#input is string and output is list of strings
+#to use the string to make a permuted list of strings in lexicographic order
+def perm_gen_lex(a):
+    newlist = []
+    if a == '':
+        return []
+    elif len(a) == 1:
+        return [a]
+    else:
+        for char in a:
+            tempstring = a.replace(char, '')            #forms a simpler string
+            for permutation in perm_gen_lex(tempstring):
+                newlist.append(char + permutation)
+    return newlist                        #adds permutation to list
 
- 
+print(perm_gen_lex('abc'))
+
