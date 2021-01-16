9dc665ebe167491632675a2ad566ba10904b6e8e
Daniel5571 <dvernola@calpoly.edu>
2021-01-16 12:27:29 -0800

    hopefully this works

--- base_convert.py
+++ base_convert.py
@@ -1,9 +1,24 @@
 #input is number and base, output is string consisting of numbers
 #takes a number and does a base conversion
 def convert(num, b):
+    print(num)
     newstring = ''
-    if num // b == 0:                 #base case
-        return newstring + str(num % b)                           #add remainder to string
+    if num // b == 0:        #base case
+        if num % b < 10:
+            return newstring + str(num % b)                           #add remainder to string
+        else:
+            if num % b == 10:
+                return newstring + 'A'
+            elif num % b == 11:
+                return newstring + 'B'
+            elif num % b == 12:
+                return newstring + 'C'
+            elif num % b == 13:
+                return newstring + 'D'
+            elif num % b == 14:
+                return newstring + 'E'
+            elif num % b == 15:
+                return newstring + 'F'
     quotient = num // b
     remainder = num % b
     if remainder == 10:
@@ -21,4 +36,4 @@ def convert(num, b):
     newstring = newstring + str(remainder)
     return convert(quotient, b) + newstring
 
-print(convert(143, 16))
+print(convert(143, 12))
--- base_convert_testcases.py
+++ base_convert_testcases.py
@@ -13,10 +13,10 @@ class TestBaseConvert(unittest.TestCase):
         self.assertEqual(convert(316,16),"13C")
 
     def test_base11(self):
-        self.assertEqual(convert(120, 11), '10A')
+        self.assertEqual(convert(120, 11), 'AA')
 
     def test_base12(self):
-        self.assertEqual(convert(143, 12), '11B')
+        self.assertEqual(convert(143, 12), 'BB')
 
     def test_base14(self):
         self.assertEqual(convert(111, 14), '7D')
@@ -26,6 +26,25 @@ class TestBaseConvert(unittest.TestCase):
 
     def test_base16_second(self):
         self.assertEqual(convert(143, 16), '8F')
+
+    def test_base16_third(self):
+        self.assertEqual(convert(11259375,16),"ABCDEF")
+
+    def test_base16_fourth(self):
+        self.assertEqual(convert(27, 16), '1B')
+
+    def test_base16_fifth(self):
+        self.assertEqual(convert(28, 16), '1C')
+
+    def test_base16_sixth(self):
+        self.assertEqual(convert(29, 16), '1D')
+
+    def test_base16_seventh(self):
+        self.assertEqual(convert(30, 16), '1E')
+
+    def test_base16_eigth(self):
+        self.assertEqual(convert(31, 16), '1F')
+
 
 if __name__ == "__main__":
         unittest.main()
