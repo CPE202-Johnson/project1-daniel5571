725fe5a06c8ecb34708e4f747e1a6b97fd717e1f
Daniel5571 <dvernola@calpoly.edu>
2021-01-16 12:52:59 -0800

    hopefully this works

--- bears.py
+++ bears.py
@@ -1,33 +1,23 @@
 #input is integer and output is either True or False (boolean)
 #tests to see if integer will pass the bears game
 def bears(n):
-    print(n)
     string = str(n)   #turns n into string so can access last two elements later on
     if n == 42:        #goal
-        print('first')
         return True
     elif n < 42:        #no longer able to achieve goal
-        print('second')
         return False
     elif n / 2 == 42:      #good
-        print('third')
         return True
     elif n % 3 == 0 and n - (int(string[-2]) * int(string[-1])) == 42:          #good
-        print('fourth')
         return True
     elif n % 4 == 0 and n - (int(string[-2]) * int(string[-1])) == 42:         #good
-        print('fifth')
         return True
     elif n % 5 == 0:
-        print('sixth')
         return bears(n - 42)
     elif n % 2 == 0:
-        print('seventh')
         return bears(int(n/2))
     elif n % 3 == 0 or n % 4 == 0:
-        print('eighth')
         return bears(n - int(string[-2]) * int(string[-1]))
     else:
-        print('ninth')
         return False
 
